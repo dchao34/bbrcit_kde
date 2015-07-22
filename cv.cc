54a0aebfa056a0768c14a2a398cd7c0fdec36ed8
dchao <dchao@caltech.edu>
2015-07-22 12:01:26 -0700

    Updated input data location.

--- cv.cc
+++ cv.cc
@@ -13,7 +13,7 @@ int main() {
   vector<double> h;
 
   // evttype1
-  ProdKde2d kde1("evttype1.dat");
+  ProdKde2d kde1("data/evttype1.dat");
   fout << "evttype1: " << endl;
   fout << endl;
   h = { 1e-1, 2e-2, 1e-2, 9e-3, 8e-3, 7.5e-3, 7e-3, 1e-3, 8e-4 };
@@ -24,7 +24,7 @@ int main() {
   fout << endl;
 
   // evttype2
-  ProdKde2d kde2("evttype2.dat");
+  ProdKde2d kde2("data/evttype2.dat");
   fout << "evttype2: " << endl;
   fout << endl;
   h = { 1e-1, 2e-2, 1e-2, 9e-3, 8e-3, 7.5e-3, 7e-3, 1e-3, 8e-4 };
@@ -35,7 +35,7 @@ int main() {
   fout << endl;
 
   // evttype3
-  ProdKde2d kde3("evttype3_sub.dat");
+  ProdKde2d kde3("data/evttype3_sub.dat");
   fout << "evttype3: " << endl;
   fout << endl;
   h = { 7e-3, 6e-3, 5.5e-3, 5.2e-3, 5e-3 };
@@ -46,7 +46,7 @@ int main() {
   fout << endl;
 
   // evttype4
-  ProdKde2d kde4("evttype4_sub.dat");
+  ProdKde2d kde4("data/evttype4_sub.dat");
   fout << "evttype4: " << endl;
   fout << endl;
   h = { 6e-3, 5.7e-3, 5.5e-3, 5.2e-3 };
@@ -57,7 +57,7 @@ int main() {
   fout << endl;
 
   // evttype5
-  ProdKde2d kde5("evttype5_sub.dat");
+  ProdKde2d kde5("data/evttype5_sub.dat");
   fout << "evttype5: " << endl;
   fout << endl;
   h = { 4.35e-4, 4e-4, 3.5e-4, 2.5e-4, 1.5e-4 }; 
