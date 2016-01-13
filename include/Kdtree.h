8163abcba77b49713b9adbd2f5bd68c6bbe861a7
dchao <dchao@caltech.edu>
2016-01-13 11:35:52 -0800

    New features for Kdtree.h:
    + Added leaf retrieval functions.
    + compute_bounding_box().

--- include/Kdtree.h
+++ include/Kdtree.h
@@ -4,10 +4,12 @@
 #include <vector>
 #include <memory>
 #include <algorithm>
+#include <limits>
 #include <queue>
 #include <iostream>
 
 #include <Point.h>
+#include <Rectangle.h>
 
 // API
 // ---
@@ -21,44 +23,103 @@ namespace bbrcit {
 template <int D, typename T>
 class Kdtree {
 
-  using point_type = Point<D,T>;
+  using PointT = Point<D,T>;
+  using IndexT = typename std::vector<PointT>::size_type;
+  using RectangleT = Rectangle<D,T>;
 
   public: 
     Kdtree();
-    Kdtree(const std::vector<point_type>&);
+    Kdtree(const std::vector<PointT>&);
     ~Kdtree();
 
-    void print() const;
+    bool empty() const;
+    void report_leaves(std::ostream&) const;
+    void report_leaves(std::vector<PointT>&) const;
+    //void print_leaves() const;
 
   private:
 
     struct Node {
       T split_ = T();
       Node *left = nullptr, *right = nullptr;
-      size_t point_idx_ = 0;
+      IndexT point_idx_ = 0;
       bool is_leaf() const { return left == nullptr && right == nullptr; }
     };
 
-    std::vector<point_type> points_;
+    std::vector<PointT> points_;
     Node *root_;
 
     Node* construct_tree(int, int, int);
     void delete_tree(Node*);
+
+    RectangleT compute_bounding_box() const;
+    void retrieve_leaves(const Node*, std::vector<IndexT>&) const;
 };
 
 // Implementations
 // ---------------
 
 template<int D, typename T>
-void Kdtree<D,T>::print() const {
+typename Kdtree<D,T>::RectangleT Kdtree<D,T>::compute_bounding_box() const {
+
+  if (points_.empty()) { return RectangleT(); }
+
+  std::vector<T> min_coord_val(D, std::numeric_limits<T>::max());
+  std::vector<T> max_coord_val(D, std::numeric_limits<T>::min());
+  for (const auto &p : points_) {
+    for (int i = 0; i < D; ++i) { 
+      min_coord_val[i] = std::min(p[i], min_coord_val[i]);
+      max_coord_val[i] = std::max(p[i], max_coord_val[i]);
+    }
+  }
+
+  RectangleT r;
+  for (int i = 0; i < D; ++i) { 
+    r.resize(i, {min_coord_val[i], max_coord_val[i]});
+  }
+
+  return r;
+}
+
+template<int D, typename T>
+inline bool Kdtree<D,T>::empty() const { return root_ == nullptr; }
+
+template<int D, typename T>
+void Kdtree<D,T>::report_leaves(std::vector<PointT> &result) const {
+
+  std::vector<IndexT> leaf_indices;
+  retrieve_leaves(root_, leaf_indices);
+  result.reserve(leaf_indices.size());
+  for (auto i : leaf_indices) { result.emplace_back(points_[i]); }
+
+}
+
+template<int D, typename T>
+void Kdtree<D,T>::report_leaves(std::ostream &os) const {
+
+  std::vector<IndexT> leaf_indices;
+  retrieve_leaves(root_, leaf_indices);
+  for (auto i : leaf_indices) { os << points_[i] << std::endl; }
+
+}
+
+template<int D, typename T>
+void Kdtree<D,T>::retrieve_leaves(const Node *r, std::vector<IndexT> &result) const {
+
+  // check for null tree
+  if (r == nullptr) { return; }
+
+  // BFS for the leaves
   std::queue<const Node*> q;
-  q.push(root_);
+  q.push(r);
   while (!q.empty()) {
     auto p = q.front(); q.pop();
-    if (p->is_leaf()) { continue; }
-    std::cout << p->split_ << " ";
-    if (p->left) { q.push(p->left); }
-    if (p->right) { q.push(p->right); }
+    if (p->is_leaf()) {
+      result.emplace_back(p->point_idx_);
+    } else {
+      if (p->left) { q.push(p->left); }
+      if (p->right) { q.push(p->right); }
+    }
   }
 }
 
@@ -69,7 +130,8 @@ template<int D, typename T>
 Kdtree<D,T>::~Kdtree() { delete_tree(root_); }
 
 template<int D, typename T>
-Kdtree<D,T>::Kdtree(const std::vector<point_type> &points) : points_(points), root_(nullptr) {
+Kdtree<D,T>::Kdtree(const std::vector<PointT> &points) : points_(points), root_(nullptr) {
+  RectangleT r = compute_bounding_box();
   if (!points_.empty()) {
     root_ = construct_tree(0, points_.size()-1, 0);
   }
@@ -83,7 +145,7 @@ typename Kdtree<D,T>::Node* Kdtree<D,T>::construct_tree(int i, int j, int d) {
   } else {
     int m = i + (j-i) / 2;
     std::nth_element(points_.begin()+i, points_.begin()+m, points_.end(), 
-                    [d] (const point_type &p1, const point_type &p2) { return p1[d] < p2[d]; });
+                    [d] (const PointT &p1, const PointT &p2) { return p1[d] < p2[d]; });
     p->split_ = points_[m][d];
     p->left = construct_tree(i, m, (d+1)%D);
     p->right = construct_tree(m+1, j, (d+1)%D);
--- test/test_kdtree.cc
+++ test/test_kdtree.cc
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <sstream>
-#include <array>
 #include <vector>
-#include <exception>
 #include <utility>
+#include <random>
+
 #include <Point.h>
 #include <Kdtree.h>
 
@@ -12,13 +11,39 @@ using bbrcit::Point;
 using bbrcit::Kdtree;
 
 int main() {
-  using Point1d = Point<1, double>;
+
+  using Point2d = Point<2, double>;
+
+  random_device rd;
+  default_random_engine g(rd());
+
+  vector<Point2d> data;
+  double start_x = -5, start_y = -10;
+  double end_x = 5, end_y = 10;
+  int N_x = 100, N_y = 100;
+  double delta_x = (end_x - start_x) / N_x;
+  double delta_y = (end_y - start_y) / N_y;
+  for (int i = 0; i < N_x; ++i) {
+    for (int j = 0; j < N_y; ++j) {
+      data.push_back(Point2d({start_x + i*delta_x, start_y + j*delta_y}));
+    }
+  }
+  cout << data.size() << endl;
+  shuffle(data.begin(), data.end(), g);
+  Kdtree<2,double> tree(data);
+
+  /*using Point1d = Point<1, double>;
+
   vector<Point1d> data;
-  for (int i = 0; i < 10; ++i) { data.push_back(Point1d({static_cast<double>(i)})); }
+  for (int i = 0; i < 100000; ++i) { data.push_back(Point1d({static_cast<double>(i)})); }
+  shuffle(data.begin(), data.end(), g);
   for (int i = 0; i < 10; ++i) { cout << data[i] << " "; } cout << endl;
+  cout << data.size() << endl;
 
   Kdtree<1,double> tree(data);
-  tree.print();
+  vector<Point1d> leaves; tree.report_leaves(leaves);
+  for (auto l : leaves) { cout << l << " "; } cout << endl;
+  */
 
   return 0;
 }
