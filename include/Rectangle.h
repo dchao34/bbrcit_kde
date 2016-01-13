f177ae3e67a7f9c549b6bafca0c9e30f5fd0539c
dchao <dchao@caltech.edu>
2016-01-12 17:08:48 -0800

    Finished Rectangle.h and added tests.
    + Rectangle.h: first time trying template template parameters...  see contains().
    + test_rectangle.h: Tests for Rectangle<>.

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -1,6 +1,7 @@
 #ifndef BBRCITKDE_RECTANGLE_H__
 #define BBRCITKDE_RECTANGLE_H__
 
+#include <cmath>
 #include <vector>
 #include <utility>
 #include <algorithm>
@@ -28,11 +29,16 @@ template <int D, typename T=double>
 class Rectangle {
 
   public: 
+
     using EdgeT = Interval<T>;
     friend void swap<>(Rectangle<D,T>&, Rectangle<D,T>&);
 
     Rectangle();
     Rectangle(const Point<D,T>&, const Point<D,T>&);
+    Rectangle(const Rectangle<D,T>&);
+    Rectangle(Rectangle<D,T>&&) noexcept;
+    Rectangle& operator=(const Rectangle<D,T>&);
+    Rectangle& operator=(Rectangle<D,T>&&) noexcept;
     ~Rectangle();
 
     // index access to edge intervals. 
@@ -42,14 +48,17 @@ class Rectangle {
     // resize the interval of the d'th dimension to e.
     void resize(size_t d, const EdgeT &e);
 
-    // returns true if the argument is fully contained in this Rectangle<>. 
-    bool contains(const Point<D,T>&) const;
-    bool contains(const Rectangle<D,T>&) const;
+    // resize this Rectangle<> to that defined by the Point<>'s in the argument.
+    void resize(const Point<D,T>&, const Point<D,T>&);
 
-    // returns the (min, max) distance from the Point<> to this Rectangle<>. 
-    T min_distance(const Point<D,T>&) const;
-    T max_distance(const Point<D,T>&) const;
-    std::pair<T,T> minmax_distance(const Point<D,T>&) const;
+    // returns true if the argument is fully contained in this Rectangle<>. 
+    // specific choices for M include Point<>, Rectangle<>
+    template <template <int D1, typename T1> class M> 
+      bool contains(const M<D,T>&) const;
+
+    // returns the (min, max) L2 distance from the Point<> to this Rectangle<>. 
+    T min_dist(const Point<D,T>&) const;
+    T max_dist(const Point<D,T>&) const;
 
   private:
 
@@ -60,6 +69,88 @@ class Rectangle {
 
 // Implementations
 // ---------------
+
+template <int D, typename T>
+void swap(Rectangle<D,T> &lhs, Rectangle<D,T> &rhs) {
+  using std::swap; swap(lhs.intervals_, rhs.intervals_);
+}
+
+template <int D, typename T>
+Rectangle<D,T>::Rectangle() : intervals_(D, EdgeT(T(), T()) ) {}
+
+template <int D, typename T>
+Rectangle<D,T>::~Rectangle() {}
+
+template <int D, typename T>
+Rectangle<D,T>::Rectangle(const Point<D,T> &p1, const Point<D,T> &p2) {
+  for (int i = 0; i < D; ++i) { 
+    auto p = std::minmax(p1[i], p2[i]);
+    intervals_.emplace_back(p.first, p.second); 
+  }
+}
+
+template <int D, typename T>
+Rectangle<D,T>::Rectangle(const Rectangle &r) : intervals_(r.intervals_) {}
+
+template <int D, typename T>
+Rectangle<D,T>::Rectangle(Rectangle &&r) noexcept : intervals_(std::move(r.intervals_)) {}
+
+template <int D, typename T>
+Rectangle<D,T>& Rectangle<D,T>::operator=(const Rectangle &r) {
+  if (this == &r) { return *this; }
+  intervals_ = r.intervals_;
+  return *this;
+}
+
+template <int D, typename T>
+Rectangle<D,T>& Rectangle<D,T>::operator=(Rectangle &&r) noexcept {
+  if (this == &r) { return *this; }
+  intervals_ = std::move(r.intervals_);
+  return *this;
+}
+
+template <int D, typename T>
+T Rectangle<D,T>::min_dist(const Point<D,T> &p) const {
+  T total = T(), curr = T();
+  for (int i = 0; i < D; ++i) {
+    curr = intervals_[i].min_dist(p[i]);
+    curr *= curr;
+    total += curr;
+  }
+  return std::sqrt(total);
+}
+
+template <int D, typename T>
+T Rectangle<D,T>::max_dist(const Point<D,T> &p) const {
+  T total = T(), curr = T();
+  for (int i = 0; i < D; ++i) {
+    curr = intervals_[i].max_dist(p[i]);
+    curr *= curr;
+    total += curr;
+  }
+  return std::sqrt(total);
+}
+
+template <int D, typename T>
+  template <template <int D1, typename T1> class M>
+bool Rectangle<D,T>::contains(const M<D,T> &obj) const {
+  bool is_contained = true;
+  for (int i = 0; i < D && is_contained; ++i) {
+    is_contained = intervals_[i].contains(obj[i]);
+  }
+  return is_contained;
+}
+
+template <int D, typename T>
+inline void Rectangle<D,T>::resize(size_t d, const EdgeT &e) { (*this)[d] = e; }
+
+template <int D, typename T>
+void Rectangle<D,T>::resize(const Point<D,T> &p1, const Point<D,T> &p2) { 
+  for (int i = 0; i < D; ++i) { 
+    auto p = std::minmax(p1[i], p2[i]);
+    intervals_[i] = EdgeT(p.first, p.second);
+  }
+}
 
 template <int D, typename T> 
 bool intersect(const Rectangle<D,T> &lhs, const Rectangle<D,T> &rhs) {
@@ -86,25 +177,6 @@ template <int D, typename T>
 inline typename Rectangle<D,T>::EdgeT& 
 Rectangle<D,T>::operator[](int i) { 
   return const_cast<EdgeT&>(static_cast<const Rectangle&>(*this)[i]);
-}
-
-template <int D, typename T>
-void swap(Rectangle<D,T> &lhs, Rectangle<D,T> &rhs) {
-  using std::swap; swap(lhs.intervals_, rhs.intervals_);
-}
-
-template <int D, typename T>
-Rectangle<D,T>::Rectangle() : intervals_(D, { T(), T() } ) {}
-
-template <int D, typename T>
-Rectangle<D,T>::~Rectangle() {}
-
-template <int D, typename T>
-Rectangle<D,T>::Rectangle(const Point<D,T> &p1, const Point<D,T> &p2) {
-  for (int i = 0; i < D; ++i) { 
-    auto p = std::minmax(p1[i], p2[i]);
-    intervals_.emplace_back(p.first, p.second); 
-  }
 }
 
 
--- test/test_rectangle.cc
+++ test/test_rectangle.cc
@@ -1,23 +1,107 @@
 #include <iostream>
 #include <utility>
 #include <Rectangle.h>
+#include <Point.h>
 
 using namespace std;
+using bbrcit::Point;
 using bbrcit::Rectangle;
 
 int main() {
 
-  Rectangle<2,double> r1, r2({0.0, 0.0}, {2.0, 2.0});
-  cout << r1 << ", " << r2 << endl;
-  swap(r1, r2);
-  cout << r1 << ", " << r2 << endl;
   cout << endl;
 
-  Rectangle<2,double> r3({1.0,1.0}, {3.0,3.0}); 
-  Rectangle<2,double> r4({3.0, 3.0}, {4.0, 4.0});
-  Rectangle<2,double> r5({0.0, 0.0}, {2.0, 2.0});
-  cout << r3 << " " << r4 << " " << r5 << endl;
-  cout << intersect(r5, r3) << " " << intersect(r5, r4) << endl;
+  // test: default constructor
+  Rectangle<2> r0;
+  cout << "+ Default constructor: " << r0 << " (cf. { (0.0, 0.0), (0.0, 0.0) }) " << endl;
+  cout << endl;
+
+  // test: two argument constructor
+  Rectangle<2> r1({0.0, 0.0}, {1.0, 1.0});
+  cout << "+ Two argument constructor: " << r1 << " (cf. { (0.0, 1.0), (0.0, 1.0) }) " << endl;
+  cout << endl;
+
+  // test: Copy/move constructor
+  Rectangle<2> r2(r1);
+  cout << "+ Copy/move constructor (1): " << r2 << " (cf. { (0.0, 1.0), (0.0, 1.0) }) " << endl;
+  Rectangle<2> temp_r1({0.0,0.0}, {1.0,1.0});
+  Rectangle<2> r3(std::move(temp_r1));
+  cout << "+ Copy/move constructor (2): " << r3 << " (cf. { (0.0, 1.0), (0.0, 1.0) }) " << endl;
+  cout << endl;
+
+  // test: Copy/move assignment
+  Rectangle<2> r4, r5({0.0, 0.0}, {1.0,1.0});
+  r4 = r5;
+  cout << "+ Copy/move assignment (1): " << r4 << " (cf. { (0.0, 1.0), (0.0, 1.0) }) " << endl;
+  Rectangle<2> temp_r2({0.0, 0.0}, {2.0,2.0});
+  r5 = std::move(temp_r2);
+  cout << "+ Copy/move assignment (2): " << r5 << " (cf. { (0.0, 2.0), (0.0, 2.0) }) " << endl;
+  cout << endl;
+
+  // test: operator[]
+  const Rectangle<2> cp0({2.0, 0.0}, {4.0, 3.0});
+  cout << "+ operator[] (1): " << "{ "<< r1[0] << ", " << r1[1] << " }" << " (cf. { (0.0, 1.0), (0.0, 1.0) }) " << endl;
+  cout << "+ operator[] (2): " << "{ "<< cp0[0] << ", " << cp0[1] << " }" << " (cf. { (2.0, 4.0), (0.0, 3.0) }) " << endl;
+  cout << endl;
+
+  // test: resize()
+  Rectangle<2> r6({0,0}, {1,1});
+  r6.resize(0, {0, 2}); r6.resize(1, {0, 2});
+  cout << "+ resize() (1): " << r6 << " (cf. { (0.0, 2.0), (0.0, 2.0) }) " << endl;
+  r6.resize({1,1}, {3,3});
+  cout << "+ resize() (2): " << r6 << " (cf. { (1.0, 3.0), (1.0, 3.0) }) " << endl;
+  cout << endl;
+
+  // test: swap()
+  r0.resize({1,1}, {3,3}); r1.resize({0,0}, {1,1});
+  swap(r0, r1);
+  cout << "+ swap() (1): " << r0 << ", " << r1;
+  cout << " (cf. { (0.0, 1.0), (0.0, 1.0) }, { (1.0, 3.0), (1.0, 3.0) }) " << endl;
+  cout << endl;
+
+  // test: contains()
+  Point<2> p0, p1({-3,0}), p2({0, 3});
+  r0.resize({-2,-2}, {2,2}); 
+  cout << "+ contains() (1): "; 
+  cout << r0.contains(p0) << ", ";
+  cout << r0.contains(p1) << ", ";
+  cout << r0.contains(p2);
+  cout << " (cf. 1, 0, 0)" << endl;
+
+  r1.resize({-1,-1}, {1,1}); r2.resize({-2,-1}, {2,1}); r3.resize({-3,-1}, {2,1}); r4.resize({3,0}, {4,1});
+  cout << "+ contains() (2): "; 
+  cout << r0.contains(r0) << ", ";
+  cout << r0.contains(r1) << ", ";
+  cout << r0.contains(r2) << ", ";
+  cout << r0.contains(r3) << ", ";
+  cout << r0.contains(r4);
+  cout << " (cf. 1, 1, 1, 0, 0)" << endl;
+  cout << endl;
+
+  // test: intersect()
+  r0.resize({-2,-2}, {2,2}); 
+  r1.resize({-1,-1}, {1,1}); r2.resize({-2,-3}, {2,-1}); r3.resize({3,-2}, {5,3});
+  cout << "+ intersect() (1): "; 
+  cout << intersect(r0, r0) << ", ";
+  cout << intersect(r0, r1) << ", ";
+  cout << intersect(r0, r2) << ", ";
+  cout << intersect(r0, r3);
+  cout << " (cf. 1, 1, 1, 0)" << endl;
+  cout << endl;
+
+  // test: min_dist(), max_dist()
+  p0.reset({0,0});
+  r1.resize({2,0}, {4,3}); 
+  cout << "+ min_dist(), max_dist() (1): " << r1.min_dist(p0) << ", " << r1.max_dist(p0) << " (c.f 2, 5)" << endl; 
+  r1.resize({1,-3}, {4,-1}); 
+  cout << "+ min_dist(), max_dist() (2): " << r1.min_dist(p0) << ", " << r1.max_dist(p0) << " (c.f 1.41421, 5)" << endl; 
+  r1.resize({-1,-1}, {1,1}); 
+  cout << "+ min_dist(), max_dist() (3): " << r1.min_dist(p0) << ", " << r1.max_dist(p0) << " (c.f 0, 1.41421)" << endl; 
+  r1.resize({0,0}, {1,1}); 
+  cout << "+ min_dist(), max_dist() (4): " << r1.min_dist(p0) << ", " << r1.max_dist(p0) << " (c.f 0, 1.41421)" << endl; 
+  r1.resize({-0.5,-0.5}, {1,1}); 
+  cout << "+ min_dist(), max_dist() (5): " << r1.min_dist(p0) << ", " << r1.max_dist(p0) << " (c.f 0, 1.41421)" << endl; 
+  cout << endl;
 
 
   return 0;
